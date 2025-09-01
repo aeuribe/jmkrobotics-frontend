09eb70697a64d0b01fe4e7cedc42e38262579fcd
Andrés Uribe <aeua2000@gmail.com>
2025-09-01 13:30:19 -0400

    feat: add contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import Contact from "../features/Contact/components/Contact";
+import Header from "../components/header/Header";
+
+export default function ContactPage() {
+  return (
+    <>
+    <Header />
+    <Contact />
+    </>
+  );
+}
