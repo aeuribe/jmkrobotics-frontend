480f5b8d052166a275072ee5a4ec81be2195018c
Andrés Uribe <aeua2000@gmail.com>
2026-05-12 13:42:50 -0400

    feat: Ascentium Capital financing calculator script to global layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -8,6 +8,7 @@ import { routing } from "@/i18n/routing";
 import { getTranslations } from "next-intl/server";
 import PipedriveChat from "./components/pipedrive/PipedriveChat";
 import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
   variable: "--font-plus-jakarta-sans",
@@ -72,6 +73,11 @@ export default async function LocaleLayout({
           <PipedriveChat key={locale} />
         </NextIntlClientProvider>
         <Analytics />
+        <Script
+          src="https://integration.financepartners.com/ascstart.js?acv=bffc42a4-96b0-47a1-a0a4-c3495d0c5da3"
+          id="acapital"
+          strategy="lazyOnload"
+        />
       </body>
     </html>
   );
